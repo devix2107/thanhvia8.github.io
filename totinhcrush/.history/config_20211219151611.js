29e50ec25791022ca2ad64915f52668bbb1e3f48
Devix <vilatui217@gmail.com>
2021-12-19 15:44:43 +0700

    web-to-tinh

---
+++ totinhcrush/.history/config_20211219151611.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+  titleWeb: "Condilon Giang",
+  introTitle: "Tên người tỏ tình (tên gì Giang nói để mình thêm vào)",
+  introDesc: `Watch thêm ing: đang xem
+              Có được anh, em đang xinh`,
+  btnIntro: "^^HiHi^^",
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰",
+  desc: "Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ",
+  btnYes: "Thích lắm <33",
+  btnNo: "Không nha :3",
+  question: "Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3",
+  btnReply: "Gửi cho bạn <3",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘",
+  messDesc: "Tối nay 7h, mình qua đón đi chơi nha.",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://github.com/zukahai/Confess-Crush", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
---
+++ totinhcrush/config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+  titleWeb: "Condilon Giang",
+  introTitle: "Tên người tỏ tình (tên gì Giang nói để mình thêm vào)",
+  introDesc: `Watch thêm ing: đang xem
+              Có được anh, em đang xinh`,
+  btnIntro: "Ấn để xem bí mật này ",
+  title: "Có yêu tao không? Trả lời nhanh",
+  // desc: "Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ",
+  btnYes: "Yêuuuu",
+  btnNo: "Ai rảnh",
+  question: "Tại sao lại yêu mình thế ạ?",
+  btnReply: "Gửi",
+  reply: "Thôi không cần trả lời đâu, mình biết mình dễ thương mà",
+  mess: "^^",
+  messDesc: "Tối nay 7h, lên phòng 303 với mình nha",
+  btnAccept: "ok ok =))) ",
+  messLink: "https://www.facebook.com/nguyenhoaithanhvi2107", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
